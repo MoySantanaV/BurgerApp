c385b2bc6a23b8977920e5af1bdc08473f73e8f3
MoySantanaV <92700261+MoySantanaV@users.noreply.github.com>
2022-08-22 17:42:00 -0500

    Formatting

--- src/Redux/ThemeMode/Mode.ts
+++ src/Redux/ThemeMode/Mode.ts
@@ -1,22 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface ModeState{
-    mode: boolean
+interface ModeState {
+  mode: boolean;
 }
 
-const initialState: ModeState={
-    mode: false,
-}
+const initialState: ModeState = {
+  mode: false,
+};
 
 export const modeSlice = createSlice({
-    name:'mode',
-    initialState,
-    reducers:{
-        changeMode: (state: ModeState ,action:{payload:boolean}) =>{
-           state.mode = !action.payload
-        }
-    }
-})
+  name: "mode",
+  initialState,
+  reducers: {
+    changeMode: (state: ModeState, action: { payload: boolean }) => {
+      state.mode = !action.payload;
+    },
+  },
+});
 
-export const {changeMode} = modeSlice.actions
-export default modeSlice.reducer
+export const { changeMode } = modeSlice.actions;
+export default modeSlice.reducer;
